8f287b27459e730249969c49d459f0d92a10426f
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-14 16:16:48 -0500

    save

--- booking.js
+++ booking.js
@@ -960,7 +960,7 @@ function initCalendar() {
             updateMaxAvailableButton();
             updateBookingSummary();
             highlightSelectedDate();
-            //setTimeout(() => disableUnavailableDates(), 50);
+            setTimeout(() => disableUnavailableDates(), 50);
         }
         
     });
