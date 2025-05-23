ee1850464abb3f5c5a821bdb08e48909ae0530f6
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-23 13:37:32 -0500

    save

--- .scripts/.bookings/functions.js
+++ .scripts/.bookings/functions.js
@@ -641,6 +641,12 @@ function formatMembershipLabel(level) {
     }
 }
 
+function setButtonText(id, text) {
+    document.querySelectorAll(`${id} .button-text`).forEach(el => {
+      el.textContent = text;
+    });
+}  
+
 // ** BOOKING SUMMARY ** //
 function updateBookingSummary() {
     const bookingDateEl = document.getElementById('booking-total-date');
