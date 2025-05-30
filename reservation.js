d47023df408a7cb602f6f093ed21a0b3ba8ed55f
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-30 15:12:21 -0500

    save

--- reservation.js
+++ reservation.js
@@ -1,4 +1,39 @@
-const hello = "hello world";
+// BOOKING GLOBALS
+
+window.bookingDetails = {
+    booking_id: null,
+    user_id: null,
+    listing_id: null,
+    location_id: null,
+    transaction_id: null,
+    status: null,
+    entry_code: null,
+    source: null,
+    type: null,
+    certificate_id: [],
+    cameras: null,
+    details: {},
+    membership: null,
+  
+    start: null,
+    end: null,
+    timezone: null,
+  
+    location_name: null,
+    location_address: null,
+  
+    base_rate: null,
+    final_rate: null,
+    rate_label: null,
+    user_credits_applied: null,
+    discounts: [],
+    discount_total: 0,
+    subtotal: null,
+    tax_rate: null,
+    tax_subtotal: null,
+    total: null
+  };
+  
 
 async function fetchBookingDetails(bookingId) {
     const { data, error } = await window.supabase
