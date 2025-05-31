c5f94de87a379a3c294c544363b0300747216c57
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-31 16:28:39 -0500

    save

--- reservation.js
+++ reservation.js
@@ -1,46 +1,10 @@
-// BOOKING GLOBALS
+const urlParams = new URLSearchParams(window.location.search);
+const bookingUuid = urlParams.get("booking");
 
-window.bookingDetails = {
-    booking_id: null,
-    user_id: null,
-    listing_id: null,
-    location_id: null,
-    transaction_id: null,
-    status: null,
-    entry_code: null,
-    source: null,
-    type: null,
-    certificate_id: [],
-    cameras: null,
-    details: {},
+if (!bookingUuid) {
+  console.warn("⚠️ No booking ID in URL.");
+}
 
-    first_name: null,
-    last_name: null,
-    email: null,
-    membership: null,
-  
-    start: null,
-    end: null,
-    duration: null,
-    timezone: null,
-  
-    location_name: null,
-    location_address: null,
-    attendees: null,
-    activities: null,
-  
-    base_rate: null,
-    final_rate: null,
-    rate_label: null,
-    user_credits_applied: null,
-    discounts: [],
-    discount_total: 0,
-    subtotal: null,
-    tax_rate: null,
-    tax_subtotal: null,
-    total: null
-  };
-  
 
 async function fetchBookingDetails(bookingId) {
     const { data, error } = await window.supabase
