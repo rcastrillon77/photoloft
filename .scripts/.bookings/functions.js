83efd88aa2e96c6bd1a7f0b78c16d2f82c221feb
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-31 16:13:26 -0500

    save

--- .scripts/.bookings/functions.js
+++ .scripts/.bookings/functions.js
@@ -2315,7 +2315,7 @@ function renderSelectedOptions() {
         return {
         id: data.id,
         ...data,
-        count: (data.count || 0) + 1
+        count: String(parseInt(data.count || "0", 10) + 1)
         };
     })
     .filter(Boolean);
@@ -2349,7 +2349,7 @@ function updatePurposeHiddenField() {
       .map(title => {
         const data = bookingTypes[title];
         if (!data) return null;
-        return { id: data.id, ...data, count: Number(data.count || 0) + 1};
+        return { id: data.id, ...data, count: String(parseInt(data.count || "0", 10) + 1)};
       })
       .filter(Boolean);
   
--- booking.js
+++ booking.js
@@ -2392,7 +2392,7 @@ function renderSelectedOptions() {
         return {
         id: data.id,
         ...data,
-        count: (data.count || 0) + 1
+        count: String(parseInt(data.count || "0", 10) + 1)
         };
     })
     .filter(Boolean);
@@ -2426,7 +2426,7 @@ function updatePurposeHiddenField() {
       .map(title => {
         const data = bookingTypes[title];
         if (!data) return null;
-        return { id: data.id, ...data, count: Number(data.count || 0) + 1};
+        return { id: data.id, ...data, count: String(parseInt(data.count || "0", 10) + 1)};
       })
       .filter(Boolean);
   
