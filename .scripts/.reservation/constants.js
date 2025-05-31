db9e951d0d7e76ce23c4697af3fcf28b82a17627
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-05-31 16:52:19 -0500

    save

--- .scripts/.reservation/constants.js
+++ .scripts/.reservation/constants.js
@@ -4,3 +4,52 @@ const bookingUuid = urlParams.get("booking");
 if (!bookingUuid) {
   console.warn("⚠️ No booking ID in URL.");
 }
+
+window.bookingGlobals = {
+  booking_uuid: null,
+  listing_uuid: null,
+  transaction_uuid: null,
+  event_uuid: [],
+  user_uuid: null,
+  
+  start: null,
+  end: null,
+  duration: null,
+  date: null,
+  timezone: null,
+  status: null,
+  cameras: true,
+  attendees: null,
+  activities: [],
+
+  transaction: {
+    base_rate: null,
+    final_rate: null,
+    rate_label: null,
+    discounts: [],
+    credits_applied: null,
+    subtotal: null,
+    tax_rate: null,
+    tax_subtotal: null,
+    total: null
+  },
+
+  user: {
+    first_name: null,
+    last_name: null,
+    email: null,
+    phone: null,
+    membership: null
+  },
+
+  listing: {
+    name: null,
+    address: {
+      address_line_1: null,
+      address_line_2: null,
+      city: null,
+      state: null,
+      zip_code: null
+    }
+  }
+};
