9009da3a6c99e2947c07936b536911e59ba0a0c4
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-02 09:33:08 -0500

    save

--- .scripts/.reservation/init.js
+++ .scripts/.reservation/init.js
@@ -14,13 +14,13 @@ async function initReservationUpdate() {
 initReservationUpdate();
 
 document.getElementById("actions_cancel").addEventListener("click", () => {
-  const refund = getRefundAmounts(bookingDetails.start, bookingDetails.transaction.total, bookingDetails.transaction.user_credits_applied);
+  const refund = getRefundAmounts(details.start, details.transaction.total, details.transaction.user_credits_applied);
   const durationStr = refund.hoursDiff >= 24
     ? `${Math.floor(refund.hoursDiff / 24)} days away`
     : `${Math.floor(refund.hoursDiff)} hours away`;
 
   document.getElementById("cancel-paragraph").innerHTML =
-    `Your reservation is ${durationStr}. Per the cancellation policy, you are eligible for a <strong>${refund.cash > 0 ? (refund.cash / bookingDetails.transaction.total) * 100 : 0}%</strong> refund or a <strong>${refund.credit > 0 ? (refund.credit / bookingDetails.transaction.total) * 100 : 0}%</strong> credit.`;
+    `Your reservation is ${durationStr}. Per the cancellation policy, you are eligible for a <strong>${refund.cash > 0 ? (refund.cash / details.transaction.total) * 100 : 0}%</strong> refund or a <strong>${refund.credit > 0 ? (refund.credit / details.transaction.total) * 100 : 0}%</strong> credit.`;
 
   const creditBtn = document.getElementById("confirm-credit-cancel");
   const cashBtn = document.getElementById("confirm-cash-cancel");
