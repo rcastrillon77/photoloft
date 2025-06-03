87d53a2b2b7ffc6f3ef31882231ade5039af6c99
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-02 22:01:39 -0500

    save

--- .scripts/.reservation/constants.js
+++ .scripts/.reservation/constants.js
@@ -1,7 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookingUuid = urlParams.get("booking");
-const { DateTime } = luxon;
 const details = null;
+
+const { DateTime } = luxon;
 const timezone = details?.listing?.timezone || 'America/Chicago';
 const CANCELLATION_WEBHOOK_URL = "https://hook.us1.make.com/vl0m26yyj1pc4hzll2aplox16qmorajg";
 
