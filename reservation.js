2c0b1a915bae5fd277c57d18b16b14c50faa51c8
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-02 21:58:09 -0500

    save

--- reservation.js
+++ reservation.js
@@ -1,15 +1,13 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookingUuid = urlParams.get("booking");
+const { DateTime } = luxon;
+const timezone = details?.listing?.timezone || 'America/Chicago';
+const CANCELLATION_WEBHOOK_URL = "https://hook.us1.make.com/vl0m26yyj1pc4hzll2aplox16qmorajg";
+let details = null;
 
 if (!bookingUuid) {
   console.warn("⚠️ No booking ID in URL.");
 }
-
-let details = null;
-const { DateTime } = luxon;
-const timezone = details?.listing?.timezone || 'America/Chicago';
-
-const CANCELLATION_WEBHOOK_URL = "https://hook.us1.make.com/vl0m26yyj1pc4hzll2aplox16qmorajg";
 
 async function rebuildBookingDetails(bookingUuid) {
   const { data: bookingData, error } = await supabase
