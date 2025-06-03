d6b03885ddee4424c0cfdb649ea88aaba62029c9
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-02 22:00:52 -0500

    save

--- reservation.js
+++ reservation.js
@@ -1,9 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookingUuid = urlParams.get("booking");
 const { DateTime } = luxon;
+const details = null;
 const timezone = details?.listing?.timezone || 'America/Chicago';
 const CANCELLATION_WEBHOOK_URL = "https://hook.us1.make.com/vl0m26yyj1pc4hzll2aplox16qmorajg";
-let details = null;
 
 if (!bookingUuid) {
   console.warn("⚠️ No booking ID in URL.");
