b4d43d28a05ebd7a13a5a755fd9894e2944d5896
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-05 10:48:39 -0500

    save

--- reservation.js
+++ reservation.js
@@ -155,6 +155,12 @@ function populateReservationDetails(details) {
 
   document.getElementById("details_paid").textContent =
     `$${(details.transaction?.total || 0).toFixed(2)}`;
+
+
+  document.getElementById("summary-date-original").textContent = DateTime.fromISO(start).setZone(timezone).toFormat('LLLL d, yyyy');
+  document.getElementById("summary-time-original").textContent = start.toFormat("h:mm a") + " to " + end.toFormat("h:mm a ZZZZ");
+  document.getElementById("summary-duration-original").textContent = details.duration + (details.duration > 1 ? " Hours" : " Hour");
+  document.getElementById("summary-rate-original").textContent = `$${details.transaction.final_rate} per Hour`;
 }
 
 function openPopup() {
