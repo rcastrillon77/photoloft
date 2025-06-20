7593ce466b1d40564b28acc6c55710940b9b1e31
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-19 23:24:10 -0500

    save: panel.js

--- panel.js
+++ panel.js
@@ -1,3 +1,10 @@
+// =======================
+// CONSTANTS
+// =======================
+const { DateTime } = luxon;
+
+const LOCATION_UUID = "9e92e73f-4f4a-4252-8d56-ebd8e74772f1";
+const TIMEZONE = "America/Chicago";
 
 
 
