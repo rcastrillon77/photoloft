7d9e67ce5b192c01ace00a4d45a1ec3f4118bf34
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-20 08:54:07 -0500

    save:

--- panel.js
+++ panel.js
@@ -8,6 +8,9 @@ const LOCATION_UUID = "9e92e73f-4f4a-4252-8d56-ebd8e74772f1";
 const TIMEZONE = "America/Chicago";
 
 
+let countdownInterval = null;
+
+
 // =======================
 // FUNCTIONS
 // =======================
