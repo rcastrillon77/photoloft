4426a48a9f89ab150c4dd5eeb1a3a236e90f64c7
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-06-20 18:48:38 -0500

    save:

--- .scripts/.panel/constants.js
+++ .scripts/.panel/constants.js
@@ -11,27 +11,6 @@ const TIMEZONE = "America/Chicago";
 let countdownInterval = null;
 
 // AUTOMATIONS
-/*
-const CAMERA_CONFIG = [
-    { id: "light-loft-back-room", position: { vertical: 140, horizontal: 180 }, label: "floor" },
-    { id: "light-loft-back-room", position: { vertical: 120, horizontal: 180 }, label: "wall" },
-    { id: "light-loft-back-room", position: { vertical: 120, horizontal: 90 }, label: "left" },
-    { id: "light-loft-back-room", position: { vertical: 120, horizontal: 270 }, label: "right" },
-    { id: "light-loft-west", position: { vertical: 110, horizontal: 140 }, label: "room" },
-    { id: "light-loft-west", position: { vertical: 135, horizontal: 140 }, label: "floor" },
-    { id: "light-loft-east", position: { vertical: 120, horizontal: 230 }, label: "door" },
-    { id: "light-loft-east", position: { vertical: 100, horizontal: 150 }, label: "room" },
-    { id: "light-loft-east", position: { vertical: 155, horizontal: 185 }, label: "tablet" }
-]
 
-  
-const LOCAL_API_BASE = "http://100.106.124.36:5000"; 
-const SNAPSHOT_MAKE_WEBHOOK_URL = "https://hook.us1.make.com/ls3kf6o5j8mml61s13v89x9cyorf5mos";
-
-*/
-
-
-const HA_WEBHOOK_PREBOOKING_URL = "https://homeassistant:8123/api/webhook/pre_booking_setup";
-
-
-  
+const HA_WEBHOOK_PREBOOKING_URL = "https://g1tsatjpileqd6zlkmhhrnlhbit9isyo.ui.nabu.casa/api/webhook/pre_booking_setup";
+const HA_WEBHOOK_SNAPSHOT_URL = "https://g1tsatjpileqd6zlkmhhrnlhbit9isyo.ui.nabu.casa/api/webhook/snapshot_trigger"; 
