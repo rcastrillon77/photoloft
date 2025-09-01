fe35496a8ffeb38035dfc592a096d637499a3ea8
rcastrillon77 <97538379+rcastrillon77@users.noreply.github.com>
2025-09-01 13:05:05 -0500

    save:

--- .scripts/.panel/constants.js
+++ .scripts/.panel/constants.js
@@ -4,7 +4,7 @@
 
 const { DateTime } = luxon;
 
-const LOCATION_UUID = "9e92e73f-4f4a-4252-8d56-ebd8e74772f1";
+const LOCATION_UUID = window.LOCATION_UUID;
 const TIMEZONE = "America/Chicago";
 
 let countdownInterval = null;
@@ -17,6 +17,5 @@ window.bookingGlobals = {};
 window.addTimeExtension = {}; 
 
 // ENDPOINTS
-const HA_WEBHOOK_PREBOOKING_URL = "https://g1tsatjpileqd6zlkmhhrnlhbit9isyo.ui.nabu.casa/api/webhook/pre_booking_setup";
-const HA_WEBHOOK_POSTBOOKING_URL = "https://g1tsatjpileqd6zlkmhhrnlhbit9isyo.ui.nabu.casa/api/webhook/post_booking_cleanup";
-const HA_WEBHOOK_SNAPSHOT_URL = "https://g1tsatjpileqd6zlkmhhrnlhbit9isyo.ui.nabu.casa/api/webhook/snapshot_trigger"; 
+const HA_WEBHOOK_PREBOOKING_URL = window.PRE_BOOKING_WEBHOOK;
+const HA_WEBHOOK_POSTBOOKING_URL = window.POST_BOOKING_WEBHOOK;
